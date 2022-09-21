14a93657bfb40413c20df9b8027a92700ba6d0a6
Javier Fernández <dogsouldev@gmail.com>
2022-09-21 10:42:17 +0200

    db and server connection try

---
+++ shymu/src/server/server.js
@@ -0,0 +1,14 @@
+// const express = require("express");
+// const dotenv = require("dotenv");
+
+// //* load env vars
+// dotenv.config({path:'../config/config.env'});
+
+// const app = express();
+
+// const PORT = process.env.PORT || 5000;
+
+// app.listen(
+// 	PORT,
+// 	console.log(`Server running is $(process.env.NODE_ENV) mode on port $(PORT)`),
+// );
